8d359ec0d2fa4fb317a09961dafd28b42dfdacfb
Yeap Soo How <sh_yeap927@live.com>
2016-09-11 06:17:08 +0000

    Commit ZSales_Order

---
+++ ZSALES_ORDER/test/integration/BusyJourneyPhone.js
@@ -0,0 +1,30 @@
+sap.ui.define([], function() {
+	"use strict";
+
+	QUnit.module("Phone busy indication");
+
+	opaTest("Should see a global busy indication while loading the metadata", function(Given, When, Then) {
+		// Arrangements
+		Given.iStartTheAppWithDelay("", 5000);
+
+		//Actions
+		When.onTheAppPage.iLookAtTheScreen();
+
+		// Assertions
+		Then.onTheAppPage.iShouldSeeTheBusyIndicator().
+		and.iTeardownMyAppFrame();
+	});
+
+	opaTest("Should see a busy indication on the master after loading the metadata", function(Given, When, Then) {
+		// Arrangements
+		Given.iStartTheAppWithDelay("", 5000);
+
+		//Actions
+		When.onTheAppPage.iWaitUntilTheBusyIndicatorIsGone();
+
+		// Assertions
+		Then.onTheMasterPage.iShouldSeeTheBusyIndicator().
+		and.iTeardownMyAppFrame();
+	});
+
+});
